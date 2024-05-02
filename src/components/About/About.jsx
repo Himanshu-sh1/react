3073c6b4da63aa50babc0fd18ffd50ffd2a8df53
Himanshu-sh1 <134672984+Himanshu-sh1@users.noreply.github.com>
2024-05-02 16:57:13 +0530

    About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-      <div className="py-16 bg-white">
+      <div className="py-16  bg-white">
           <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
               <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                   <div className="md:5/12 lg:w-5/12">
@@ -16,13 +16,14 @@ export default function About() {
                           React development is carried out by passionate developers
                       </h2>
                       <p className="mt-6 text-gray-600">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                          accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                          aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                       Using React.js for web development may be extremely convenient because React is one of those cases
+                       where you get the hang of a single technology to reuse it across a range of platforms easily. 
+                       And all due to being a natural library, with the primary purpose of creating separate web design 
+                       elements and components (anything from buttons and labels to grids and interactive features).
                       </p>
                       <p className="mt-4 text-gray-600">
-                          Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                          Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                       React can be used to build web, mobile, and desktop applications, making it a versatile framework 
+                       for cross-platform development.
                       </p>
                   </div>
               </div>
